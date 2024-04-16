1ad180632a264824ccff54101df10cf34dd1fb1b
William Zhang <willzhang73@gmail.com>
2024-04-16 18:15:00 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,17 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+insert into "users" ("username", "hashedPwd")
+values('will', '123'),
+      ('James', '123');
+
+insert into "recipes" 
+("userId", "title", "imageUrl", "preparationTime", "cuisine", "glutenFree", "vegetarian", "ingredients", "instructions")
+values('1','sushi', 'test', '30', 'Japanese', true, true, 'rice, sashimi, seaweed', 'cook, roll, eat');
+
+insert into "comments" ("userId", "recipeId", "message")
+values('1', '1', 'good recipe!');
+
+insert into "likes" ("userId", "recipeId")
+values ('1', '1');
+
--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,55 @@ set client_min_messages to warning;
 
 -- DANGER: this is NOT how to do it in the real world.
 -- `drop schema` INSTANTLY ERASES EVERYTHING.
-drop schema "public" cascade;
+ drop schema "public" cascade; 
 
 create schema "public";
+CREATE TABLE "public"."likes" (
+  "likesId" serial,
+  "userId" integer,
+  "recipeId" integer,
+  "createdAt" timestamp,
+  primary key ("likesId")
+);
+
+CREATE TABLE "public"."recipes" (
+  "recipeId" serial,
+  "userId" integer,
+  "title" text not null,
+  "imageUrl" text not null,
+  "preparationTime" integer not null,
+  "cuisine" text not null,
+  "glutenFree" boolean,
+  "vegetarian" boolean,
+  "ingredients" text,
+  "instructions" text,
+  "createdAt" timestamp,
+  primary key ("recipeId")
+);
+
+CREATE TABLE "public"."users" (
+  "userId" serial,
+  "username" text not null,
+  "hashedPwd" text not null,
+  "createdAt" timestamp,
+  primary key ("userId")
+);
+
+CREATE TABLE "public"."comments" (
+  "commentId" serial,
+  "userId" integer,
+  "recipeId" integer,
+  "message" text not null,
+  "createdAt" timestamp,
+    primary key ("commentId")
+);
+
+ALTER TABLE "likes" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "likes" ADD FOREIGN KEY ("recipeId") REFERENCES "recipes" ("recipeId");
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "comments" ADD FOREIGN KEY ("recipeId") REFERENCES "recipes" ("recipeId");
